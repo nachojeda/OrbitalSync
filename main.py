30720a4bd45796cb0dd6377d82ef0970f827d794
nachojeda <ojeda.gen.ai@gmail.com>
2025-05-24 18:47:22 +0200

    TLE function implemented

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import os
-
+from fastapi import FastAPI, HTTPException
 from utils import (
-    all_satellites
+    tle
 )
+from dotenv import load_dotenv
+
+load_dotenv()
 
 N2YO_API_KEY = os.getenv('N2YO_API_KEY')
 N2YO_USER = os.getenv('N2YO_USER')
 URL = 'https://api.n2yo.com/rest/v1/satellite/'
+TLE = '/tle/{id}'
+SATELLITE_POSITIONS = '/positions/{id}/{observer_lat}/{observer_lng}/{observer_alt}/{seconds}'
+VISUAL_PASSES = '/visualpasses/{id}/{observer_lat}/{observer_lng}/{observer_alt}/{days}/{min_visibility}'
+RADIO_PASSES = '/visualpasses/{id}/{observer_lat}/{observer_lng}/{observer_alt}/{days}/{min_visibility}'
+# Galileo Id = 22
 
-all_satellites(URL, N2YO_USER, N2YO_API_KEY)
+id=22
+tle(URL+f'tle/{id}544&apiKey='+N2YO_API_KEY, N2YO_USER, N2YO_API_KEY)
+
+# app = FastAPI()
+
+# @app.get("/ny2c/tle", tags=['TLE'])
+# async def tle() -> JSONResponse:
--- utils.py
+++ utils.py
@@ -1,11 +1,34 @@
 import requests
 from requests.auth import HTTPBasicAuth
 
-def all_satellites(URL: str, N2YO_USER: str, N2YO_API_KEY: str):
+def tle(URL: str, N2YO_USER: str, N2YO_API_KEY: str):
+    """
+    """
+    basic = HTTPBasicAuth(N2YO_USER, N2YO_API_KEY)
+
+    r = requests.get(url=URL, auth=basic)
+    print(r.content)
+
+def satellite_positions(URL: str, N2YO_USER: str, N2YO_API_KEY: str):
     """
     """
     basic = HTTPBasicAuth(N2YO_USER, N2YO_API_KEY)
 
     r = requests.get(url=URL, auth=basic)
     print(r)
-    print(r.status_code)
+
+def visual_passes(URL: str, N2YO_USER: str, N2YO_API_KEY: str):
+    """
+    """
+    basic = HTTPBasicAuth(N2YO_USER, N2YO_API_KEY)
+
+    r = requests.get(url=URL, auth=basic)
+    print(r)
+
+def radio_passes(URL: str, N2YO_USER: str, N2YO_API_KEY: str):
+    """
+    """
+    basic = HTTPBasicAuth(N2YO_USER, N2YO_API_KEY)
+
+    r = requests.get(url=URL, auth=basic)
+    print(r)
